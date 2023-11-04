971d96d1ce4ac9a12f8ad934cce1c88da162a766
nived-cv <nived.cv@vonnue.com>
2023-11-04 18:42:50 +0530

    setup  initial

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const Header = () => {
+
+
+    return(<div className = "header-container">
+        <div className='left-items'>
+            <h1></h1>
+        </div>
+    </div>)
+}
